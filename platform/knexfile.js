f379ea68437c3be3f3dff9b5692c14348e0d3b38
James Miller <james.rex.miller.4@gmail.com>
2020-01-30 13:30:11 -0700

    update knex config file and add production environment config

--- platform/knexfile.js
+++ platform/knexfile.js
@@ -6,44 +6,19 @@ module.exports = {
     client: 'pg',
     connection: 'postgres://localhost/tweets',
     migrations: {
-      directory: './db/migrations'
+      directory: './db/migrations',
     },
     seeds: {
-      directory:'./db/seeds/'
+      directory: './db/seeds/',
     },
     useNullAsDefault: true,
   },
-}
-//   staging: {
-//     client: 'postgresql',
-//     connection: {
-//       database: 'my_db',
-//       user:     'username',
-//       password: 'password'
-//     },
-//     pool: {
-//       min: 2,
-//       max: 10
-//     },
-//     migrations: {
-//       tableName: 'knex_migrations'
-//     }
-//   },
-
-//   production: {
-//     client: 'postgresql',
-//     connection: {
-//       database: 'my_db',
-//       user:     'username',
-//       password: 'password'
-//     },
-//     pool: {
-//       min: 2,
-//       max: 10
-//     },
-//     migrations: {
-//       tableName: 'knex_migrations'
-//     }
-//   }
-
-// };
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL + `?ssl=true`,
+    migrations: {
+      directory: './db/migrations',
+    },
+    useNullAsDefault: true,
+  },
+};
