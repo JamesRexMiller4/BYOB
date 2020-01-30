5d9f1b830175d8f3912b9bc22fdc34bef4c1d657
James Miller <james.rex.miller.4@gmail.com>
2020-01-30 14:06:38 -0700

    Add knex file to root dir

---
+++ knexfile.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL + `?ssl=true`,
+    migrations: {
+      directory: './db/migrations',
+    },
+    useNullAsDefault: true,
+  }
+}
