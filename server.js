e3266b9ff27e22f7605348d7b4a18a72f6cee717
James Miller <james.rex.miller.4@gmail.com>
2020-01-29 22:23:23 -0700

    Add working GET route handler for api/v1/users, add non-working post

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const environment = process.env.NODE_ENV || 'development';
 const configuration = require('./platform/knexfile')[environment];
 const database = require('knex')(configuration);
 
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 3001);
 
 
 app.get('/api/v1/tweets', async (req, res) => {
@@ -16,6 +16,38 @@ app.get('/api/v1/tweets', async (req, res) => {
   }
 });
 
+app.get('/api/v1/users', async (req, res) => {
+  try {
+    const users = await database('users').select();
+    res.status(200).json(users)
+  } catch(error) {
+    res.status(500).json({ error });
+  }
+});
+
+app.post('/api/v1/users', async (req, res) => {
+  const user = req.body;
+  console.log(user)
+
+  for (let requiredParameter of ['username', 'handle']) {
+    if (!user[requiredParameter]) {
+      return res
+        .status(422)
+        .send({ error: `Expected format: { username: <String>, handle: <String> }. You're missing a ${requiredParameter} property`});
+    }
+  }
+
+  try {
+    const id = await database('users').insert(user, 'id');
+    res.status(201).json({ id })
+  } 
+  
+  catch(error) {
+    res.status(500).json({ error });
+  }
+});
+
+
 app.listen(app.get('port'), () => {
   console.log(`Server is listening on localhost:${app.get('port')}...`)
-})
+});
