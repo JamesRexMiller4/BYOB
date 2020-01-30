67ba189cea91dd86c3554ade2f7441ea1055436e
James Miller <james.rex.miller.4@gmail.com>
2020-01-29 20:31:39 -0700

    Require in database and environmnent and write get request for tweets

--- server.js
+++ server.js
@@ -1,7 +1,20 @@
 const express = require('express');
 const app = express();
+const environment = process.env.NODE_ENV || 'development';
+const configuration = require('./platform/knexfile')[environment];
+const database = require('knex')(configuration);
 
 app.set('port', process.env.PORT || 3000);
+
+
+app.get('/api/v1/tweets', async (req, res) => {
+  try {
+    const tweets = await database('tweets').select();
+    res.status(200).json(tweets)
+  } catch(error) {
+    res.status(500).json({ error });
+  }
+});
 
 app.listen(app.get('port'), () => {
   console.log(`Server is listening on localhost:${app.get('port')}...`)
